b586009a5bb93d79d5bca8cdc46b5ac659def68c
rzarzafernandez <rzarzafernandez@eniac-01.danielcastelao.org>
2014-12-15 08:50:36 +0100

    comit2

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -8,27 +8,30 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
-System.out.println("Es numero primo");
-else
- System.out.println("NO es numero primo");           	 
+        int iN = 11;
+        
+        if (metodoMio(iN))
+            System.out.println("Es numero primo");
+        else
+            System.out.println("NO es numero primo");           	 
           	 
-for (int x=2;x<1000;x++) {
-if (metodoMio(x))
-System.out.print(x + " ");                         	 
-}         	 
-	}
-public static boolean metodoMio(int variable){
-int j = 2;
-boolean aux=true;
-while ((aux) && (j!=variable)){
-if (variable % j == 0)
-aux = false;
-j++;
-}
-return aux;
-  } 	 
+        for (int x=2;x<1000;x++) {
+            if (metodoMio(x))
+                System.out.print(x + " ");                         	 
+        }         	 
+    }
+    
+    public static boolean metodoMio(int variable){
+        int j = 2;
+        boolean aux=true;
+        
+        while ((aux) && (j!=variable)){
+            if (variable % j == 0)
+                aux = false;
+                j++;
+        }
+        return aux;
+    } 	 
     
 }
 
